760419ce260734e0229590b5005ec12dda2df52a
samuelcrtz <78769720+samuelcrtz@users.noreply.github.com>
2021-03-26 17:51:07 -0600

    Update MassShootings.py

--- MassShootings.py
+++ MassShootings.py
@@ -7,28 +7,10 @@ ms = pd.read_excel('MASS SHOOTINGS.xlsx')
 
 ms.shape
 
-ms.rename(columns = {'S#':'Caso_No.'}, inplace= True)
-ms.rename(columns = {'Title':'Titulo'}, inplace= True)
-ms.rename(columns = {'Location':'Lugar'}, inplace= True)
-ms.rename(columns = {'Date':'Dia'}, inplace= True)
-ms.rename(columns = {'civilian intervention':'Intervencion_civil'}, inplace= True)
-ms.rename(columns = {'police intervention':'Intervencion_policial'}, inplace= True)
-ms.rename(columns = {'suicide':'Suicidio'}, inplace= True)
-ms.rename(columns = {'age.1':'Edad'}, inplace= True)
-ms.rename(columns = {'motive':'Motivo'}, inplace= True)
-ms.rename(columns = {'Summary':'Resumen'}, inplace= True)
-ms.rename(columns = {'Fatalities':'Muertes'}, inplace= True)
-ms.rename(columns = {'Injured':'Heridos'}, inplace= True)
-ms.rename(columns = {'Total victims':'Victimas_Totales'}, inplace= True)
-ms.rename(columns = {'Mental Health Issues':'Problemas_de_Salud_Mental'}, inplace= True)
-ms.rename(columns = {'Race':'Raza'}, inplace= True)
-ms.rename(columns = {'Gender':'Genero'}, inplace= True)
-ms.rename(columns = {'Latitude':'Latitud'}, inplace= True)
-ms.rename(columns = {'Longitude':'Longitud'}, inplace= True)
-ms.rename(columns = {'STATE':'Estado'}, inplace= True)
-ms.rename(columns = {'GUNCONTROL':'Control_de_armas'}, inplace= True)
-ms.rename(columns = {'STRICT':'Estrictos'}, inplace= True)
-ms.rename(columns = {'NOT STRICT':'No_Estrictos'}, inplace= True)
+ms.rename(columns = {'S#':'Caso_No.', 'Title':'Titulo', 'Location':'Lugar', 'Date':'Dia', 'civilian intervention':'Intervencion_civil', 'police intervention':'Intervencion_policial'}, inplace= True)
+ms.rename(columns = {'suicide':'Suicidio', 'age.1':'Edad', 'motive':'Motivo','Summary':'Resumen', 'Fatalities':'Muertes','Injured':'Heridos','Total victims':'Victimas_Totales', 'Mental Health Issues':'Problemas_de_Salud_Mental'}, inplace= True)
+ms.rename(columns = {'Race':'Raza', 'Gender':'Genero', 'Latitude':'Latitud', 'Longitude':'Longitud','STATE':'Estado','GUNCONTROL':'Control_de_armas','STRICT':'Estrictos','NOT STRICT':'No_Estrictos'}, inplace= True)
+
 
 ms.drop(['age','SCHOOL', 'GANG', 'CRIME', 'ALTERCATION','POLITICAL','FAMILY', 'RANDOM','WORK', 'WELL', 'ILL', 'latino', 'native am', 'asian', 'black', 'white', 'Na', 'NaN', 'NAA', 'NAAA', 'male','female','0G', '1G', '2G', '3G', '4G', '5G', '7G', '8G', '9G', '10G'], axis=1, inplace=True)
 
